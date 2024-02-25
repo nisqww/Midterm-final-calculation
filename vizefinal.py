62f9908a234eb97b0eb0a506b4e83101fd058348
Nisa <116450295+nisqww@users.noreply.github.com>
2024-02-25 12:48:36 +0300

    Add files via upload

--- vizefinal.py
+++ vizefinal.py
@@ -1,3 +1,4 @@
+
 midterm_weight = int(input("What percentage will the midterm exam weigh in total grade? "))
 final_weight = int(input("What percentage will the final exam weigh in total grade?"))
 midterm_grade = int(input("Please enter your midterm grade: "))
@@ -13,3 +14,22 @@ if total_grade >= 50:
     print("You passed the course.")
 else:
     print("You failed the course. ")
+
+if 80<=total_grade<=100:
+    print("AA")
+elif 71<=total_grade<80:
+    print("BA")
+elif 63<=total_grade<71:
+    print("BB")
+elif 55<=total_grade<63:
+    print("CB")
+elif 50<=total_grade<55:
+    print("CC")
+elif 45<=total_grade<50:
+    print("DC")
+elif 35<=total_grade<45:
+    print("DD")
+elif 0<=total_grade<35:
+    print("FF")
+
+
